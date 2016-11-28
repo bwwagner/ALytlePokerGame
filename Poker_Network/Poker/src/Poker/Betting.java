d1353faec4c149a8d59d2f43bc7d77f32281913c
Branden Wagner <bwwagner@indiana.edu>
2016-11-27 20:58:00 -0500

    Completed Betting System

--- Poker_Network/Poker/src/Poker/Betting.java
+++ Poker_Network/Poker/src/Poker/Betting.java
@@ -10,15 +10,24 @@ package Poker;
 public class Betting {
     private int jackpot = 0;
     private int ante = 0;
+    private int myPot = 0;
     
     //Blind betting mode
     private int largeBlind;
     private int smallBlind;
     
     public Betting(int newPot, int newAnte){
+        //Constructor used for Ante Betting Mode
         jackpot = newPot;
         ante = newAnte;
-    }  // end Constructor
+    }  // end Ante Constructor
+    
+    public Betting(int newPot, int newLargeBl, int newSmallBl){
+        //Constructor used for Blind Betting Mode
+        jackpot = newPot;
+        largeBlind = newLargeBl;
+        smallBlind = newSmallBl;
+    } // end Blind Constructor
     
     public int getLarge(){
         return this.largeBlind;
@@ -32,12 +41,24 @@ public class Betting {
         return this.ante;
     }
     
+    public int getBank(){
+        return myPot;
+    }
+    
     public void setAnte(int amount){
         this.ante = amount;
     } // end setAnte
     
+    public void winner(){
+        this.myPot += this.jackpot;
+        this.jackpot = 0;
+    }
+    
     public void bet(int amount){
-        if (amount > 0) jackpot += amount;
+        if (myPot > 0 && amount > 0 && amount <= myPot) {
+            jackpot += amount;
+            myPot -= amount;
+        }
         else return;
     }// end bet
     
